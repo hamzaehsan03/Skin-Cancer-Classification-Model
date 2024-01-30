3337489706d95056452115d89316e03b85c016c1
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-01-30 00:56:36 +0000

    Finding out relevant tags for the DICOM file on individual image

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import numpy as np
+import pydicom
+import os
+
+current_directory = os.getcwd()
+dcm_dir = os.path.join(current_directory, "data")
+
+
+dicom_file = pydicom.read_file(dcm_dir, "train\\ISIC_0052212.dcm")
+print(dicom_file)

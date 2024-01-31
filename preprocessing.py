759ff194e182cce207f97e8da65e121954b82ea3
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-01-31 20:07:16 +0000

    Implemented visualisations for normalised images

--- preprocessing.py
+++ preprocessing.py
@@ -16,10 +16,8 @@ def read_dcm(path, img_size = (224, 224)):
     dicom = pydicom.read_file(path)
     data = apply_voi_lut(dicom.pixel_array, dicom)
 
-    # If the dicom data is stored in a photometric interpretation of MONOCHROME1
-    # Invert the image through subtracting the maximum pixel value from each pixel
-    if dicom.PhotometricInterpretation == 'MONOCHROME1':
-        data = np.amax(data) - data
+    if len(data.shape) == 2:
+        data = np.stack((data) * 3, axis = -1)
 
     # Shift the pixel values of the image by subtracting the minimum pixel value in the image
     # This will cause the lowest pixel value to become 0
@@ -28,19 +26,35 @@ def read_dcm(path, img_size = (224, 224)):
     # Scale the pixel values to a range from 0 to 1 by dividing it by the maximum pixel value
     # Essentially normalising the image intensity
     data = data / np.max(data)
-
     # Scale the image to an 8-bit image
     data = (data * 255).astype(np.uint8)
-    return tf.image.resize(data, img_size)
+    plt.imshow(data, cmap='gray')
+    plt.show()
+    resized_image = tf.image.resize(data, img_size)
+    resized_image = tf.cast(resized_image, tf.uint8)
+    return resized_image
     
-def process_image(image_path, metadata):
+def process_image(image_path, metadata, visualise=False):
     image = read_dcm(image_path)
+    plt.imshow(image, cmap='gray')
+    plt.show()
     base_name = os.path.splitext(os.path.basename(image_path))[0]
     row = metadata[metadata['image_name'] == base_name]
     label = row['target'].values[0] if not row.empty else -1
+
+    if visualise:
+        plt.imshow(image)
+        plt.title(f'Label: {label}')
+        plt.show()
+
     return image, label
 
 def main():
+
+    sample_paths = [os.path.join(".\\data\\train", filename) for filename in os.listdir(".\\data\\train")
+                    if filename.endswith(".dcm")][:5]
+    for path in sample_paths:
+        process_image(path, metadata, visualise=True)
 
     def parallel_load_images(folder, metadata):
         image_paths = [os.path.join(folder, filename) for filename in os.listdir(folder) if filename.endswith(".dcm")]
@@ -54,12 +68,6 @@ def main():
     dcm_dir = os.path.join(current_directory, "data")
     train_images = parallel_load_images(".\\data\\train", metadata)
     test_images = parallel_load_images(".\\data\\test", metadata)
-
-    plt.figure(figsize=(10, 10))
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(train_images[i])
-        plt.axis("off")
 
 if __name__ == "__main__":
     main()

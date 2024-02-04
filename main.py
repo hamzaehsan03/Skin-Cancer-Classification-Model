dde97f68f9ec03415e0fc9183c22cd39e7fb9f92
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-02-04 19:35:49 +0000

    Implemented VGG19 Model for classification Included checks for GPU CUDA compatability

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ dcm_dir = os.path.join(current_directory, "data\\train\\ISIC_0052212.dcm")
 
 dicom_file = pydicom.read_file(dcm_dir)
 print(dicom_file)
+
+import tensorflow as tf
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
--- vgg19_model.py
+++ vgg19_model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+import datetime
 from keras.applications import VGG19
-from tensorflow.python.keras.models import Model 
-from tensorflow.python.keras.layers import Dense, Flatten, Dropout
+from keras.models import Model 
+from keras.layers import Dense, Flatten, Dropout
+from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
 
 # Load VGG19 model trained on ImageNet
@@ -9,13 +11,51 @@ from keras.preprocessing.image import ImageDataGenerator
 # Freeze the weights of the model during initial training
 
 base_model = VGG19(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+base_model.summary()
 for layer in base_model.layers:
     layer.trainable = False
 
+#import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+
+# Add custom layers to the model
+# Flatten the model to a single vector
+# Add a connected layer with relu activation
 x = Flatten()(base_model.output)
 x = Dense(1024, activation='relu')(x)
 x = Dropout(0.5)(x)
 x = Dense(1, activation='sigmoid')(x)
 
 model = Model(base_model.input, x)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+train_data = ImageDataGenerator(1./255)
+train_gen = train_data.flow_from_directory(
+    'data/train',
+    target_size = (224, 224),
+    batch_size = 32,
+    class_mode = 'binary'
+)
+
+validation_data = ImageDataGenerator(1./255)
+validation_gen = validation_data.flow_from_directory(
+    'data/validation',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='binary'
+)
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+callbacks = [
+    EarlyStopping(patience=5, monitor='val_loss'),
+    ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
+    TensorBoard(log_dir=log_dir, histogram_freq=1)
+]
+
+history = model.fit(
+    train_gen,
+    epochs=10, 
+    validation_data=validation_gen,
+    callbacks=callbacks
+)
